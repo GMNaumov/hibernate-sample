847ac6b2717d6adad6e33f2f640d9bd99daa278d
Georgii Naumov <gmnaumov@yandex.ru>
2022-12-05 22:51:01 +0300

    feat:Hibernate settings, main class

---
+++ src/main/java/com/gmnaumov/projects/hiber/MainApp.java
@@ -0,0 +1,13 @@
+package com.gmnaumov.projects.hiber;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class MainApp {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration()
+                .buildSessionFactory();
+
+        sessionFactory.close();
+    }
+}
